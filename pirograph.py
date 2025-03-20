493a947a21eaf27ffa8879ea5440882fdf32af54
jjsanderson <jonathan@quernstone.com>
2025-03-20 08:44:34 +0000

    Comment out input handling.

--- pirograph.py
+++ pirograph.py
@@ -72,7 +72,7 @@ def main():
         # (or might give a horrid update cadence, we'll see)
         if frame_count % frame_window == 0:
             frame_stats(time_begin, time_start, frame_count, frame_window)
-            handle_inputs()
+            # handle_inputs()
             current_gain = pirocam.analogue_gain
             # Increase exposure
             pirocam.analogue_gain = current_gain + 0.1
